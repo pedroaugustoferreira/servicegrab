2c59c28a90270bb0db8b5e369e79df63964b8930
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2020-09-10 10:46:12 -0300

    Create install.sh

---
+++ linux/install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -l
+mkdir /var/log/servicegrab 2> /dev/null
+cd /var/log/servicegrab
+curl -O https://raw.githubusercontent.com/pedroaugustoferreira/servicegrab/master/linux/main.sh
+chmod +x main.sh
+cat /var/spool/cron/tabs/root |grep -v "servicegrab" > /tmp/root.cron
+cat /tmp/root.cron > /var/spool/cron/tabs/root
+echo "* * * * * /var/log/servicegrab/main.sh &> /var/log/servicegrab/main.log" >> /var/spool/cron/tabs/root
