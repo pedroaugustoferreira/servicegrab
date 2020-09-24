d44da9c0a1cf176285f5e300e67d52bf6a419dc6
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2020-09-23 22:50:05 -0300

    Update main.sh

--- linux/main.sh
+++ linux/main.sh
@@ -6,7 +6,7 @@ ls $remove 2> /dev/null |grep -v "^01_"| awk '{print "rm -rf "$1}' | sh
 
 gzip -f *.txt 2> /dev/null
 
-comandos=( "uptime" "df -h" "mount" "ps -ef" "ifconfig" "fdisk -l" "systemctl" "systemctl status" "dmidecode" "rpm -qa" "free -g" "netstat -nlp" "uname -a" "dmesg -c" "pstree" "lsblk" )
+comandos=( "uptime" "df -h" "mount" "ps -ef" "ifconfig" "fdisk -l" "systemctl" "systemctl status" "dmidecode" "rpm -qa" "free -g" "netstat -nlp" "uname -a" "dmesg -c" "pstree" "lsblk" "cat /proc/meminfo" )
 
 dt=$(date "+%d_%m_%Y_")
 
