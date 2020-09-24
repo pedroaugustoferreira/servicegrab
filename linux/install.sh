dd73c69f5bd5f987d7b4e815bf4a39fe87d54fd0
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2020-09-24 08:08:12 -0300

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,10 +1,23 @@
 #! /bin/bash -l
 # curl -s https://raw.githubusercontent.com/pedroaugustoferreira/servicegrab/master/linux/install.sh | bash -s --
 
+cat /etc/*rele*|egrep "CentOS Linux 7" &> /dev/null
+if [ "$?" -eq "0"  ]; then
+    cronfile=/var/spool/cron/root
+fi
+
+if [ -z "$cronfile" ];
+then
+   echo "SO nao identificado"
+   exit 1
+fi
+
 mkdir /var/log/servicegrab 2> /dev/null
 cd /var/log/servicegrab
 curl -O https://raw.githubusercontent.com/pedroaugustoferreira/servicegrab/master/linux/main.sh
 chmod +x main.sh
-cat /var/spool/cron/tabs/root |grep -v "servicegrab" > /tmp/root.cron
-cat /tmp/root.cron > /var/spool/cron/tabs/root
-echo "0 6 * * * /var/log/servicegrab/main.sh &> /var/log/servicegrab/main.log" >> /var/spool/cron/tabs/root
+
+cat $cronfile |grep -v "servicegrab" > /tmp/root.cron
+
+cat /tmp/root.cron > $cronfile
+echo "0 6 * * * /var/log/servicegrab/main.sh &> /var/log/servicegrab/main.log" >> $cronfile
