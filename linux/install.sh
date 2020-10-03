86f955c9eb9e32f644f820b28105b5f499382667
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2020-10-03 09:57:07 -0300

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,27 +1,54 @@
 #! /bin/bash -l
 # curl -s https://raw.githubusercontent.com/pedroaugustoferreira/servicegrab/master/linux/install.sh | bash -s --
+check()
+{ 
+    if [ "$1" -eq "0"  ]; then echo "success"; else echo "error"; exit 1; fi
+}
 
-cat /etc/*rele* 2> /dev/null | egrep "CentOS Linux 7|Red Hat Enterprise Linux Server release 7" &> /dev/null
-if [ "$?" -eq "0"  ]; then
-    cronfile=/var/spool/cron/root
-fi
+start()
+{
+    set -x
+    #-------------------------------
+    echo "(1) *** Arquivo cron rood ***"
+    cat /etc/*rele* 2> /dev/null | egrep "CentOS Linux 7|Red Hat Enterprise Linux Server release 7" &> /dev/null
+    if [ "$?" -eq "0"  ]; then
+        cronfile=/var/spool/cron/root
+    fi
 
-if [ -z "$cronfile" ];
-then
-   echo "SO nao identificado"
-   exit 1
-fi
+    if [ -z "$cronfile" ];
+    then
+       echo "SO nao identificado"
+       exit 1
+    fi
+    check $?
+    #-------------------------------
+    echo "(2) *** Criando pasta /var/log/servicegrab ***"
+    echo "---Criando pasta /var/log/servicegrab"
+    mkdir /var/log/servicegrab 2> /dev/null
+    cd /var/log/servicegrab
+    check $?
+    #-------------------------------
+    echo "(3) *** Download main.sh ***"
+    curl -O https://raw.githubusercontent.com/pedroaugustoferreira/servicegrab/master/linux/main.sh &> /dev/null
+    cat main.sh |grep servicegrab
+    check $?
+    #-------------------------------
+    echo "(4) *** chmod +x main.sh ***"
+    chmod +x main.sh
+    check $?
+    #-------------------------------
+    echo "(5) *** add crontab ***"
+    cat $cronfile |grep -v "servicegrab" > /tmp/root.cron
+    cat /tmp/root.cron > $cronfile
+    check $?
+    echo "# servicegrab" >> $cronfile
+    check $?
+    echo "0 6 * * * /var/log/servicegrab/main.sh &> /var/log/servicegrab/main.log" >> $cronfile
+    check $?
+    crontab -l |grep servicegrab
+    check $?
+    ls -la /var/log/servicegrab/main.sh
+    check $?
+}
 
-mkdir /var/log/servicegrab 2> /dev/null
-cd /var/log/servicegrab
-curl -O https://raw.githubusercontent.com/pedroaugustoferreira/servicegrab/master/linux/main.sh
-chmod +x main.sh
-
-cat $cronfile |grep -v "servicegrab" > /tmp/root.cron
-
-cat /tmp/root.cron > $cronfile
-echo "# servicegrab" >> $cronfile
-echo "0 6 * * * /var/log/servicegrab/main.sh &> /var/log/servicegrab/main.log" >> $cronfile
-
-crontab -l |grep servicegrab
-ls -la /var/log/servicegrab/
+start 2>&1 |egrep -v "for|start|grep|env|cut|echo|\+\+|sed|\[|cat|check|curl|exit"
